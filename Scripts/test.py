b1602345f0f068219271365635eb5ea8a50a88ae
Ludvig <38407332+Ludvigdfl@users.noreply.github.com>
2025-04-09 10:11:28 +0100

    Update test.py

--- Scripts/test.py
+++ Scripts/test.py
@@ -5,34 +5,22 @@ import datetime
 import time
 import json
 
-TOKEN = os.getenv("GITHUB_TOKEN")
-if not TOKEN:
-    print("❌ GitHub TOKEN token not found. Make sure you're running this in GitHub Actions.")
-    exit(1)
- 
 
-OPEN_AI_API = os.getenv("OPEN_AI_API")
-if not OPEN_AI_API:
-    print("❌ GitHub OPEN_AI_API token not found. Make sure you're running this in GitHub Actions.")
-    exit(1)
-
- 
-with open(file=r"Scripts/Commentary.json", mode="r") as File:
-    Frames = json.load(File)
-
-Commentary=''
-for Frame in Frames:
-    Commentary += f'\n\n{Frame["Frame_Commentary"]}'
+def Get_Tokens():
     
-print(Commentary)
+    TOKEN = os.getenv("GITHUB_TOKEN")
+    if not TOKEN:
+        print("❌ GitHub TOKEN token not found. Make sure you're running this in GitHub Actions.")
+        exit(1)
+     
+    
+    OPEN_AI_API = os.getenv("OPEN_AI_API")
+    if not OPEN_AI_API:
+        print("❌ GitHub OPEN_AI_API token not found. Make sure you're running this in GitHub Actions.")
+        exit(1)
 
-FILE_TMP  = "Scripts/Commentary_TEMP.txt" 
-
-with open(file= FILE_TMP, mode="w") as File:
-    File.write(Commentary)
-
-
-
+    return TOKEN, OPEN_AI_API
+ 
 def Read_File(File_Name, FileType):
 
     if(FileType == 'json'):
@@ -64,42 +52,56 @@ def Write_File_Tmp(File_Name_TMP, FileType, File_Content):
 
 
 
-def Write_File(File_Name, FileType):
+def Write_File(File_Name, FileType, TOKEN):
 
     REPO_OWNER   = "Ludvigdfl"
     REPO_NAME    = "Climber-Vasaloppet"
     FILE         =  File_Name
     BRANCH       = "main"   
+    
+    url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/Scripts/{FILE}"
 
     if(FileType == 'txt'):
-        url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/Scripts/{FILE}"
-        
-        # Read the data to be commited to the repo - can be in a temp-folder in the runnint container
         with open(FILE_TMP, "rb") as File:
             Content_File = base64.b64encode(File.read()).decode()
-        
-        # Check if file already exists (needed for updates)
-        headers = {"Authorization": f"Bearer {TOKEN}", "Accept": "application/vnd.github.v3+json"}
-        response = requests.get(url, headers=headers)
-        
-        # Prepare data for upload
-        data = {
-            "message": "Upload audio via GitHub Actions",
-            "content": Content_File,
-            "branch": BRANCH
-        }
-        
-        if response.status_code == 200:
-            # File exists, update it
-            sha = response.json().get("sha")  # Get the SHA of the existing file
-            data["sha"] = sha  # Add SHA to the data for the update
-            response = requests.put(url, json=data, headers=headers)
-        else:
-            # File does not exist, create it
-            response = requests.put(url, json=data, headers=headers)
-        
-        # Check response status
-        if response.status_code in [200, 201]:
-            print("✅ Data successfully uploaded to GitHub!")
-        else:
-            print(f"❌ Failed to upload Data: {response.json()}")
+     
+    else:
+        with open(FILE_TMP, "rb") as File:
+            Content_File = base64.b64encode(json.load(File).decode()   
+
+                                            
+    # Check if file already exists (needed for updates)
+    headers = {"Authorization": f"Bearer {TOKEN}", "Accept": "application/vnd.github.v3+json"}
+    response = requests.get(url, headers=headers)
+    
+    # Prepare data for upload
+    data = {
+        "message": "Upload audio via GitHub Actions",
+        "content": Content_File,
+        "branch": BRANCH
+    }
+    
+    if response.status_code == 200:
+        # File exists, update it
+        sha = response.json().get("sha")  # Get the SHA of the existing file
+        data["sha"] = sha  # Add SHA to the data for the update
+        response = requests.put(url, json=data, headers=headers)
+    else:
+        # File does not exist, create it
+        response = requests.put(url, json=data, headers=headers)
+    
+    # Check response status
+    if response.status_code in [200, 201]:
+        print("✅ Data successfully uploaded to GitHub!")
+    else:
+        print(f"❌ Failed to upload Data: {response.json()}")
+
+
+
+##################################
+############## MAIN ##############
+def main():
+    
+    TOKEN, OPEN_AI_API = Get_Tokens()
+
+main()
