4b9294abaaffa77dbbf5e20935db2e611caad192
Ludvig <38407332+Ludvigdfl@users.noreply.github.com>
2025-04-09 07:19:05 +0100

    Update test.py

--- Scripts/test.py
+++ Scripts/test.py
@@ -33,40 +33,56 @@ with open(file= FILE_TMP, mode="w") as File:
 
 
 
+def Read_File(File_Name, FileType):
 
-REPO_OWNER   = "Ludvigdfl"
-REPO_NAME    = "Climber-Vasaloppet"
-FILE         = "Commentary.txt"
-BRANCH       = "main"   
+    if(FileType == 'json'):
+        with open(file = File_Name, mode="r") as File:
+           Data = json.load(File)
+            
+        return Data
+        
+    if(FileType == 'txt'):
+        with open(file = File_Name, mode="r") as File:
+           Data = File.read()
+            
+        return Data
 
-url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/Scripts/{FILE}"
+def Write_File(File_Name, FileType):
 
-# Read and encode the audio file
-with open(FILE_TMP, "rb") as File:
-    Content_File = base64.b64encode(File.read()).decode()
+    REPO_OWNER   = "Ludvigdfl"
+    REPO_NAME    = "Climber-Vasaloppet"
+    FILE         =  File_Name
+    BRANCH       = "main"   
 
-# Check if file already exists (needed for updates)
-headers = {"Authorization": f"Bearer {TOKEN}", "Accept": "application/vnd.github.v3+json"}
-response = requests.get(url, headers=headers)
-
-# Prepare data for upload
-data = {
-    "message": "Upload audio via GitHub Actions",
-    "content": Content_File,
-    "branch": BRANCH
-}
-
-if response.status_code == 200:
-    # File exists, update it
-    sha = response.json().get("sha")  # Get the SHA of the existing file
-    data["sha"] = sha  # Add SHA to the data for the update
-    response = requests.put(url, json=data, headers=headers)
-else:
-    # File does not exist, create it
-    response = requests.put(url, json=data, headers=headers)
-
-# Check response status
-if response.status_code in [200, 201]:
-    print("✅ Data successfully uploaded to GitHub!")
-else:
-    print(f"❌ Failed to upload Data: {response.json()}")
+    if(FileType == 'txt'):
+        url = f"https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/contents/Scripts/{FILE}"
+        
+        # Read the data to be commited to the repo - can be in a temp-folder in the runnint container
+        with open(FILE_TMP, "rb") as File:
+            Content_File = base64.b64encode(File.read()).decode()
+        
+        # Check if file already exists (needed for updates)
+        headers = {"Authorization": f"Bearer {TOKEN}", "Accept": "application/vnd.github.v3+json"}
+        response = requests.get(url, headers=headers)
+        
+        # Prepare data for upload
+        data = {
+            "message": "Upload audio via GitHub Actions",
+            "content": Content_File,
+            "branch": BRANCH
+        }
+        
+        if response.status_code == 200:
+            # File exists, update it
+            sha = response.json().get("sha")  # Get the SHA of the existing file
+            data["sha"] = sha  # Add SHA to the data for the update
+            response = requests.put(url, json=data, headers=headers)
+        else:
+            # File does not exist, create it
+            response = requests.put(url, json=data, headers=headers)
+        
+        # Check response status
+        if response.status_code in [200, 201]:
+            print("✅ Data successfully uploaded to GitHub!")
+        else:
+            print(f"❌ Failed to upload Data: {response.json()}")
